d2ea1c441ec38bd5e26ac1bcdc513dec663f5aeb
ZacByrnes <oldwallaby@gmail.com>
2018-08-21 15:42:14 +1000

    add_practical

---
+++ prac_04/password.py
@@ -0,0 +1,37 @@
+"""
+CP1404/CP5632 - Practical - Suggested Solution
+Get a password with minimum length and display asterisks
+"""
+
+#Pre Notes:
+#Some Password lines will need to be repeated when working with the lengths.
+
+
+MINIMUM_LENGTH = 4
+
+
+def version_1():
+    password = input("Enter password of at least {} characters: ".format(MINIMUM_LENGTH))
+    while len(password) < MINIMUM_LENGTH:
+        password = input("Enter password of at least {} characters: ".format(MINIMUM_LENGTH))
+
+    print('*' * len(password))
+
+def main():
+    password = get_password(MINIMUM_LENGTH)
+    print_asterisks(password)
+
+
+def get_password(minimum_length):
+    password = input("Enter password of at least {} characters: ".format(minimum_length))
+    while len(password) < minimum_length:
+        print("Error: Password too short")
+        password = input("Enter password of at least {} characters: ".format(minimum_length))
+    return password
+
+
+def print_asterisks(sequence):
+    print('*' * len(sequence))
+
+
+main()
