7fc1eb6d90fe29d406f12d01fd76e9a694ffd775
jc493773 <Oldwallaby2>
2018-09-04 15:51:42 +1000

    add_practical

---
+++ prac_06/guitar.py
@@ -0,0 +1,31 @@
+from prac_06.guitars import Guitar
+
+def main():
+    guitars = []
+    print("My guitars!")
+    name = input("Name: ")
+    year = int(input("Year: "))
+    cost = float(input("Cost: "))
+    guitars_to_add = Guitar(name, year, cost)
+    guitars.append(guitars_to_add)
+    print(guitars_to_add, "added")
+    name = input("Name: ")
+
+    guitar.append(Guitar("Gibson L-5 CES", 1922, 16035.50))
+    guitar.append(Guitar("Line 6 JTV-59", 2010, 1512.90))
+
+    guitars.sort()
+    print("These are my guitars: ")
+
+    if guitars is not None:
+        for i, guitar in enumerate(guitars):
+            vintage_string = ""
+            if guitar.is_vintage():
+                vintage_string = "(vintage)"
+            print("Guitar {0}: {1.name:>30} ({1.year}), worth ${1.cost:10,.2f}
+             {2}".format(i + 1, guitar, vintage_string))")
+        else:
+            print("No guitars :( Quick, go and buy one!")
+
+main()
+
