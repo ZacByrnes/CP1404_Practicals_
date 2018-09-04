1526033c56a53a48f987c27dfffcc31653789e21
jc493773 <Oldwallaby2>
2018-09-04 15:38:08 +1000

    add_practical

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -2,15 +2,32 @@
 CP1404/CP5632 Practical - Suggested Solution
 Guitar program.
 """
+PRESENT YEAR = 2018
+VINTAGE_AGE = 50
 
+class Guitar:
+    def main():
+        name = input("Enter Name: ")
+        #Always Error Check
+        while name != "":
+            year = int(input("Year: "))
+            cost = float(input("Cost: $"))
+            print("My guitar: {}, first made in {} costed ${}".format(name, year, cost))
 
-def main():
-    
-    name = input("Enter Name: ")
-    #Always Error Check
-    while name != "":
-        year = int(input("Year: "))
-        cost = float(input("Cost: $"))
-        print("My guitar: {}, first made in {1} costed {2}".format(name, year, cost))
+    def __init__(self, name, year=0, cost=0):
+        self.name = name
+        self.year = year
+        self.cost = cost
 
-main()
+    def __str__(self, ):
+        return "{} ({}) : ${}".format(self.name, self.year, self.cost)
+
+    def get_age(self, ):
+        return PRESENT_YEAR - self.year
+
+    def is_vintage(self):
+        return self.get_age() >= VINTAGE_AGE
+
+    def __lt(self, other):
+        return self.year < other.year
+    main()
